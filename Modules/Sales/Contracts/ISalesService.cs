63fb0dc1998a194902bede8a78e58c6ef540e9ee
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-07-27 12:58:39 -0300

    feat[modules]: Sales (base)

---
+++ Modules/Sales/Contracts/ISalesService.cs
@@ -0,0 +1,6 @@
+namespace ECommerce_ASP_NET_API.Modules.Sales.Contracts;
+
+public interface ISalesService
+{
+
+}
---
+++ Modules/Sales/DTOs/SalesDTO.cs
@@ -0,0 +1,6 @@
+namespace ECommerce_ASP_NET_API.Modules.Sales.DTOs;
+
+public class SalesDTO
+{
+
+}
