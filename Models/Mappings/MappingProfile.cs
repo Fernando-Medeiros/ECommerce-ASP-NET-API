c5b8c3a0cb2ea9c964ed7c091996e6f38acb4248
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-07-27 21:45:56 -0300

    feat[models]: MappingProfile (CreateMap(*Models))-> IMPL

--- Models/Mappings/MappingProfile.cs
+++ Models/Mappings/MappingProfile.cs
@@ -1,6 +1,20 @@
+using AutoMapper;
+using ECommerce_ASP_NET_API.Modules.Cart.DTOs;
+using ECommerce_ASP_NET_API.Modules.Category.DTOs;
+using ECommerce_ASP_NET_API.Modules.Customer.DTOs;
+using ECommerce_ASP_NET_API.Modules.Product.DTOs;
+using ECommerce_ASP_NET_API.Modules.Sales.DTOs;
+
 namespace ECommerce_ASP_NET_API.Models.Mappings;
 
-public class MappingProfile
+public class MappingProfile : Profile
 {
-
+    public MappingProfile()
+    {
+        CreateMap<Cart, CartDTO>().ReverseMap();
+        CreateMap<Category, CategoryDTO>().ReverseMap();
+        CreateMap<Customer, CustomerDTO>().ReverseMap();
+        CreateMap<Product, ProductDTO>().ReverseMap();
+        CreateMap<Sales, SalesDTO>().ReverseMap();
+    }
 }
