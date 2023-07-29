194ad3b1deb8d37d0c799f19fcfb86a8f2b619dc
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-07-28 22:46:13 -0300

    feat[module]: CustomerRepository

--- Modules/Customer/CustomerRepository.cs
+++ Modules/Customer/CustomerRepository.cs
@@ -1,6 +1,47 @@
 namespace ECommerce_ASP_NET_API.Modules.Customer;
 
-public class CustomerRepository
-{
+using Microsoft.EntityFrameworkCore;
+using ECommerce_ASP_NET_API.Context;
+using ECommerce_ASP_NET_API.Models;
+using ECommerce_ASP_NET_API.Modules.Customer.Contracts;
 
+
+public class CustomerRepository : ICustomerRepository
+{
+    private readonly DatabaseContext _context;
+    public CustomerRepository(DatabaseContext context) => _context = context;
+
+    public async Task<Customer?> FindById(string id)
+    {
+        return await _context.Customers
+            .AsNoTracking()
+            .SingleOrDefaultAsync(c => c.Id == id);
+    }
+
+    public async Task<Customer> Create(Customer customer)
+    {
+        _context.Customers.Add(customer);
+
+        await _context.SaveChangesAsync();
+
+        return customer;
+    }
+
+    public async Task<Customer> Update(Customer customer)
+    {
+        _context.Customers.Entry(customer).State = EntityState.Modified;
+
+        await _context.SaveChangesAsync();
+
+        return customer;
+    }
+
+    public async Task<Customer> Remove(Customer customer)
+    {
+        _context.Customers.Remove(customer);
+
+        await _context.SaveChangesAsync();
+
+        return customer;
+    }
 }
