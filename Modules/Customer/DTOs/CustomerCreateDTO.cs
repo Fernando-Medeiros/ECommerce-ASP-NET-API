fcc9e0a8fef554c671f1357d6ba7b0aa9b5c3362
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-07-28 22:43:51 -0300

    feat[module]: Customer -> [Base, Create, Update, Query]( DTOs )

---
+++ Modules/Customer/DTOs/CustomerCreateDTO.cs
@@ -0,0 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace ECommerce_ASP_NET_API.Modules.Customer.DTOs;
+
+public class CustomerCreateDTO : CustomerDTO
+{
+    [JsonIgnore]
+    public override string? Id { get; set; }
+
+    [Required(ErrorMessage = "The Name is Required")]
+    public override string? Name { get; set; }
+
+    [Required(ErrorMessage = "The FirstName is Required")]
+    public override string? FirstName { get; set; }
+
+    [Required(ErrorMessage = "The LastName is Required")]
+    public override string? LastName { get; set; }
+
+    [Required(ErrorMessage = "The Email is Required")]
+    public override string? Email { get; set; }
+
+    [Required(ErrorMessage = "The Password is Required")]
+    public override string? Password { get; set; }
+
+    [JsonIgnore]
+    public override DateTime? CreatedAt { get; set; }
+}
--- Modules/Customer/DTOs/CustomerDTO.cs
+++ Modules/Customer/DTOs/CustomerDTO.cs
@@ -1,6 +1,30 @@
 namespace ECommerce_ASP_NET_API.Modules.Customer.DTOs;
 
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using ECommerce_ASP_NET_API.Models;
+
 public class CustomerDTO
 {
+    public virtual string? Id { get; set; }
 
+    [MinLength(3), MaxLength(50)]
+    public virtual string? Name { get; set; }
+
+    [MinLength(3), MaxLength(50)]
+    public virtual string? FirstName { get; set; }
+
+    [MinLength(3), MaxLength(50)]
+    public virtual string? LastName { get; set; }
+
+    [MinLength(11), MaxLength(150)]
+    public virtual string? Email { get; set; }
+
+    [MinLength(8), MaxLength(255)]
+    public virtual string? Password { get; set; }
+
+    public virtual DateTime? CreatedAt { get; set; }
+
+    [JsonIgnore]
+    public ICollection<Cart>? Carts { get; set; }
 }
