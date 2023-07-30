6a0768c08224bb4bb3a9395b193895ab19e96349
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-07-29 22:43:00 -0300

    feat[module]: Category -> [Base, Create, Update, Query]( DTOs )

--- Modules/Category/DTOs/CategoryDTO.cs
+++ Modules/Category/DTOs/CategoryDTO.cs
@@ -1,6 +1,18 @@
 namespace ECommerce_ASP_NET_API.Modules.Category.DTOs;
 
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using ECommerce_ASP_NET_API.Models;
+
 public class CategoryDTO
 {
+    public virtual int? Id { get; set; }
 
+    [MinLength(3), MaxLength(100)]
+    public virtual string? Name { get; set; }
+
+    public virtual DateTime? CreatedAt { get; set; }
+
+    [JsonIgnore]
+    public virtual ICollection<Product>? Products { get; set; }
 }
