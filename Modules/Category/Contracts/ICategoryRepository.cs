e108b0f936e1a8112e51e3afcf60e167ea527b3c
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-07-29 22:44:14 -0300

    feat[module]: Category -> ICategoryRepository & ICategoryService ( Contract )

--- Modules/Category/Contracts/ICategoryRepository.cs
+++ Modules/Category/Contracts/ICategoryRepository.cs
@@ -1,6 +1,14 @@
 namespace ECommerce_ASP_NET_API.Modules.Category.Contracts;
 
+using ECommerce_ASP_NET_API.Models;
+
 public interface ICategoryRepository
 {
+    public IQueryable<Category> Find();
 
+    public Task<Category> Create(Category category);
+
+    public Task<Category> Update(Category category);
+
+    public Task<Category> Remove(Category category);
 }
--- Modules/Category/Contracts/ICategoryService.cs
+++ Modules/Category/Contracts/ICategoryService.cs
@@ -1,6 +1,16 @@
 namespace ECommerce_ASP_NET_API.Modules.Category.Contracts;
 
+using ECommerce_ASP_NET_API.Modules.Category.DTOs;
+
 public interface ICategoryService
 {
+    public Task<IEnumerable<CategoryDTO>> FindMany(CategoryQueryDTO query);
 
+    public Task<CategoryDTO> FindOne(int id);
+
+    public Task<CategoryDTO> Register(CategoryDTO category);
+
+    public Task Update(CategoryDTO category);
+
+    public Task Remove(CategoryDTO category);
 }
