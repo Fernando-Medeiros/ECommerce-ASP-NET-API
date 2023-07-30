63d68771e7c1dfc3779f7c0d7895f0e069c74f1a
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-07-30 19:38:53 -0300

    feat[exceptions]: HttpResponseExceptionFilter

--- Startup/ProviderController.cs
+++ Startup/ProviderController.cs
@@ -1,10 +1,15 @@
+using ECommerce_ASP_NET_API.Exceptions;
+
 namespace ECommerce_ASP_NET_API.Startup;
 
 public static partial class ServiceProviders
 {
     public static void Controller(WebApplicationBuilder builder)
     {
-        builder.Services.AddControllers();
+        builder.Services.AddControllers(options =>
+            { options.Filters.Add<HttpResponseExceptionFilter>(); }
+        );
+
         builder.Services.AddEndpointsApiExplorer();
     }
 }
