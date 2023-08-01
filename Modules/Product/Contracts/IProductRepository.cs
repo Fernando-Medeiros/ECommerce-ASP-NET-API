2f89dee6408c4911629c736e4fd52ecce7223bfb
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-08-01 13:23:40 -0300

    feat[module]: Product -> (Contracts) [Repository, Service]

--- Modules/Product/Contracts/IProductRepository.cs
+++ Modules/Product/Contracts/IProductRepository.cs
@@ -1,6 +1,17 @@
 namespace ECommerce_ASP_NET_API.Modules.Product;
 
+using ECommerce_ASP_NET_API.Models;
+
 public interface IProductRepository
 {
+    public Task<IEnumerable<Product>> FindMany(ProductQueryDTO query);
 
+    public Task<Product?> FindOne(int? id = null, string? name = null);
+
+    public Task<Product> Create(Product product);
+
+    public Task<Product> Update(Product product);
+
+    public Task<Product> Remove(Product product);
 }
+
