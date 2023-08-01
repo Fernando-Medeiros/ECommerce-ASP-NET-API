b854d4cde9faaf55966f3ceb73c2409a859742b9
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-08-01 18:58:53 -0300

    feat[build]: Injectable -> AddScoped -> Cart [Service & Repository]

--- Startup/ProviderInjectable.cs
+++ Startup/ProviderInjectable.cs
@@ -1,3 +1,4 @@
+using ECommerce_ASP_NET_API.Modules.Cart;
 using ECommerce_ASP_NET_API.Modules.Category;
 using ECommerce_ASP_NET_API.Modules.Customer;
 using ECommerce_ASP_NET_API.Modules.Customer.Contracts;
@@ -9,10 +10,15 @@ public static partial class ServiceProviders
 {
     public static void Injectable(WebApplicationBuilder builder)
     {
+        builder.Services.AddScoped<ICartRepository, CartRepository>();
+        builder.Services.AddScoped<ICartService, CartService>();
+
         builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
         builder.Services.AddScoped<ICategoryService, CategoryService>();
+
         builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
         builder.Services.AddScoped<ICustomerService, CustomerService>();
+
         builder.Services.AddScoped<IProductRepository, ProductRepository>();
         builder.Services.AddScoped<IProductService, ProductService>();
     }
