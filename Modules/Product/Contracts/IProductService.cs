18e1b3ade14a4614c5f3a920e23fd32311701b69
Fernando Medeiros <97191840+Fernando-Medeiros@users.noreply.github.com>
2023-08-01 13:38:08 -0300

    Feat[module]: Product (#10)
    * refactor[module]: Product -> Namespace
    
    * feat[build]: Injectable -> AddScoped -> Product [Service & Repository]:
    
    * feat[models]: Product -> Add CategoryId
    
    * feat[module]: Product -> (DTOs) [Base, Create, Update, Query]
    
    * feat[module]: Product -> (Contracts) [Repository, Service]
    
    * feat[module]: ProductRepository
    
    * feat[module]: ProductService
    
    * feat[module]: ProductController

--- Modules/Product/Contracts/IProductService.cs
+++ Modules/Product/Contracts/IProductService.cs
@@ -1,6 +1,14 @@
-namespace ECommerce_ASP_NET_API.Modules.Product.Contracts;
+namespace ECommerce_ASP_NET_API.Modules.Product;
 
 public interface IProductService
 {
+    public Task<IEnumerable<ProductDTO>> FindMany(ProductQueryDTO query);
 
+    public Task<ProductDTO> FindOne(int? id = null, string? name = null);
+
+    public Task<ProductDTO> Register(ProductDTO product);
+
+    public Task Update(ProductDTO product);
+
+    public Task Remove(ProductDTO product);
 }
--- Modules/Product/DTOs/ProductDTO.cs
+++ Modules/Product/DTOs/ProductDTO.cs
@@ -1,6 +1,31 @@
-namespace ECommerce_ASP_NET_API.Modules.Product.DTOs;
+namespace ECommerce_ASP_NET_API.Modules.Product;
+
+using ECommerce_ASP_NET_API.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 public class ProductDTO
 {
+    public virtual int? Id { get; set; }
 
+    [MinLength(3), MaxLength(100)]
+    public virtual string? Name { get; set; }
+
+    [MinLength(5), MaxLength(255)]
+    public virtual string? Description { get; set; }
+
+    [MinLength(5), MaxLength(255)]
+    public virtual string? ImageURL { get; set; }
+
+    public virtual decimal? Price { get; set; }
+
+    [Range(1, 9999)]
+    public virtual long? Stock { get; set; }
+
+    public virtual int? CategoryId { get; set; }
+
+    public virtual DateTime? CreatedAt { get; set; }
+
+    [JsonIgnore]
+    public virtual Category? Category { get; set; }
 }
