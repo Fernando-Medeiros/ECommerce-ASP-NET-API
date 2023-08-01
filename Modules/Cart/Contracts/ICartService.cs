e5a4b85f9efcf1b22f8b8b54b49696cdc1266537
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-08-01 19:02:13 -0300

    feat[module]: Cart -> (Contracts) [Repository, Service]

--- Modules/Cart/Contracts/ICartService.cs
+++ Modules/Cart/Contracts/ICartService.cs
@@ -1,6 +1,12 @@
-namespace ECommerce_ASP_NET_API.Modules.Cart.Contracts;
+namespace ECommerce_ASP_NET_API.Modules.Cart;
 
 public interface ICartService
 {
+    public Task<CartDTO?> FindOne(int? id);
 
+    public Task<CartDTO> Register(CartDTO cart);
+
+    public Task Update(CartDTO cart);
+
+    public Task Remove(CartDTO cart);
 }
