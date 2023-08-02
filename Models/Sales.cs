43564a153eaca65ef2cfcc0207987a5fd1c1503b
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-08-02 17:11:14 -0300

    fix[models]: Sales -> ProductId(string) to (int)

--- Models/Sales.cs
+++ Models/Sales.cs
@@ -4,7 +4,7 @@ public class Sales
 {
     public int Id { get; set; }
     public string? CustomerId { get; set; }
-    public string? ProductId { get; set; }
+    public int ProductId { get; set; }
     public decimal Price { get; set; }
     public int Quantity { get; set; }
     public DateTime CreatedAt { get; set; }
