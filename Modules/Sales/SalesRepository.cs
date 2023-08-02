95004367e4fbe67c841741cb72f769990734c1c8
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-08-02 17:37:30 -0300

    feat[module]: SalesRepository

--- Modules/Sales/SalesRepository.cs
+++ Modules/Sales/SalesRepository.cs
@@ -1,6 +1,42 @@
 namespace ECommerce_ASP_NET_API.Modules.Sales;
 
-public class SalesRepository
-{
+using ECommerce_ASP_NET_API.Context;
+using ECommerce_ASP_NET_API.Models;
+using Microsoft.EntityFrameworkCore;
 
+public class SalesRepository : ISalesRepository
+{
+    private readonly DatabaseContext _context;
+    public SalesRepository(DatabaseContext context) => _context = context;
+
+    public async Task<Sales?> Find(int? id)
+    {
+        if (id != null)
+            return await _context.Sales
+                .AsNoTracking()
+                .SingleOrDefaultAsync(s => s.Id == id);
+
+        return null;
+    }
+
+    public async Task Create(Sales sales)
+    {
+        _context.Add(sales);
+
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task Update(Sales sales)
+    {
+        _context.Update(sales).State = EntityState.Modified;
+
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task Remove(Sales sales)
+    {
+        _context.Remove(sales);
+
+        await _context.SaveChangesAsync();
+    }
 }
