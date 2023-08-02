d1ea596ab5027d16c848c313f4b0c030022be37e
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-08-02 17:36:52 -0300

    feat[module]: Sales -> (Contracts) [Repository, Service]

--- Modules/Sales/Contracts/ISalesService.cs
+++ Modules/Sales/Contracts/ISalesService.cs
@@ -2,5 +2,9 @@ namespace ECommerce_ASP_NET_API.Modules.Sales;
 
 public interface ISalesService
 {
+    public Task<SalesDTO> Find(int? id);
 
+    public Task Register(SalesDTO sales);
+
+    public Task Remove(SalesDTO sales);
 }
