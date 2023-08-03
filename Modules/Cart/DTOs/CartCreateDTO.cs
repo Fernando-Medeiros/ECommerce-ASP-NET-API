46b00ad1a7a3ac9637d6227e74a00c2a494ecdeb
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-08-03 19:52:41 -0300

    refactor[module]: Cart -> DTOs

--- Modules/Cart/DTOs/CartCreateDTO.cs
+++ Modules/Cart/DTOs/CartCreateDTO.cs
@@ -1,22 +1,13 @@
 namespace ECommerce_ASP_NET_API.Modules.Cart;
 
 using System.ComponentModel.DataAnnotations;
-using System.Text.Json.Serialization;
 
-public class CartCreateDTO : CartDTO
+public class CartCreateDTO
 {
-    [JsonIgnore]
-    public override int? Id { get; set; }
-
-    [Required(ErrorMessage = "The Customer is Required")]
-    public override string? CustomerId { get; set; }
-
     [Required(ErrorMessage = "The Product is Required")]
-    public override int? ProductId { get; set; }
+    public int ProductId { get; set; }
 
     [Required(ErrorMessage = "The Quantity is Required")]
-    public override int? Quantity { get; set; }
-
-    [JsonIgnore]
-    public override DateTime? CreatedAt { get; set; }
+    [Range(1, 999)]
+    public int Quantity { get; set; }
 }
