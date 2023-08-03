82eb71a6e16d1c1ae081e9853f9060c761aa5c9e
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-08-03 10:18:11 -0300

    feat[build]: Injectable: Session [Service, Repository, TokenService, EnviromentDTO]

--- Startup/ProviderInjectable.cs
+++ Startup/ProviderInjectable.cs
@@ -3,6 +3,7 @@ using ECommerce_ASP_NET_API.Modules.Category;
 using ECommerce_ASP_NET_API.Modules.Customer;
 using ECommerce_ASP_NET_API.Modules.Product;
 using ECommerce_ASP_NET_API.Modules.Sales;
+using ECommerce_ASP_NET_API.Modules.Session;
 
 namespace ECommerce_ASP_NET_API.Startup;
 
@@ -24,5 +25,13 @@ public static partial class ServiceProviders
 
         builder.Services.AddScoped<ISalesRepository, SalesRepository>();
         builder.Services.AddScoped<ISalesService, SalesService>();
+
+        builder.Services.AddScoped<ISessionRepository, SessionRepository>();
+        builder.Services.AddScoped<ISessionService, SessionService>();
+
+        builder.Services.Configure<EnvironmentDTO>(
+            builder.Configuration.GetSection("Environment"));
+
+        builder.Services.AddTransient<TokenService>();
     }
 }
