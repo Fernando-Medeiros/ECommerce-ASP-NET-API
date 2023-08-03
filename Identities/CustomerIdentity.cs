e589e0e8eabd0941661a1702aa5fb2621c49a6f3
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-08-03 17:30:00 -0300

    feat[identities]: CustomerIdentity

---
+++ Identities/CustomerIdentity.cs
@@ -0,0 +1,22 @@
+namespace ECommerce_ASP_NET_API.Identities;
+
+using System.Security.Claims;
+using ECommerce_ASP_NET_API.Exceptions;
+
+public class CustomerIdentity
+{
+    public readonly string Id;
+
+    public CustomerIdentity(ClaimsPrincipal principal)
+    {
+        Id = ExtractProperty(principal, "id").Value;
+    }
+
+    private static Claim ExtractProperty(ClaimsPrincipal principal, string keySelector)
+    {
+        return principal.Claims
+            .SingleOrDefault(c => c.Type.Equals(keySelector))
+            ?? throw new ForbiddenError("Access denied, token incompatible");
+    }
+
+}
