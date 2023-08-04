85bf45500c610e78854acd13b19bb0b708984300
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-08-04 12:00:14 -0300

    refactor[module]: ProductRepository

--- Modules/Product/Contracts/IProductRepository.cs
+++ Modules/Product/Contracts/IProductRepository.cs
@@ -8,10 +8,10 @@ public interface IProductRepository
 
     public Task<Product?> FindOne(int? id = null, string? name = null);
 
-    public Task<Product> Create(Product product);
+    public Task Create(Product product);
 
-    public Task<Product> Update(Product product);
+    public Task Update(Product product);
 
-    public Task<Product> Remove(Product product);
+    public Task Remove(Product product);
 }
 
