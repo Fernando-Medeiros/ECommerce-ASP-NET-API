27e34e231eec8e8eadc148f61ad372b58143c4a3
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-08-04 12:00:45 -0300

    refactor[module]: ProductService

--- Modules/Product/Contracts/IProductService.cs
+++ Modules/Product/Contracts/IProductService.cs
@@ -6,7 +6,7 @@ public interface IProductService
 
     public Task<ProductDTO> FindOne(int? id = null, string? name = null);
 
-    public Task<ProductDTO> Register(ProductDTO product);
+    public Task Register(ProductDTO product);
 
     public Task Update(ProductDTO product);
 
