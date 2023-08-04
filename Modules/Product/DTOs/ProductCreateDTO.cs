3d7c441d72b031a0c0cf18f23bbb1c4fe3f471b6
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-08-04 11:59:15 -0300

    refactor[module]: Product (DTOs)

--- Modules/Product/DTOs/ProductCreateDTO.cs
+++ Modules/Product/DTOs/ProductCreateDTO.cs
@@ -1,31 +1,28 @@
 namespace ECommerce.Modules.Product;
 
 using System.ComponentModel.DataAnnotations;
-using System.Text.Json.Serialization;
 
-public class ProductCreateDTO : ProductDTO
+public class ProductCreateDTO
 {
-    [JsonIgnore]
-    public override int? Id { get; set; }
-
     [Required(ErrorMessage = "The Name is Required")]
-    public override string? Name { get; set; }
+    [MinLength(3), MaxLength(100)]
+    public string? Name { get; set; }
 
     [Required(ErrorMessage = "The Description is Required")]
-    public override string? Description { get; set; }
+    [MinLength(5), MaxLength(255)]
+    public string? Description { get; set; }
 
     [Required(ErrorMessage = "The ImageURL is Required")]
-    public override string? ImageURL { get; set; }
+    [MinLength(5), MaxLength(255)]
+    public string? ImageURL { get; set; }
 
     [Required(ErrorMessage = "The Price is Required")]
-    public override decimal? Price { get; set; }
+    public decimal Price { get; set; }
 
     [Required(ErrorMessage = "The Stock is Required")]
-    public override long? Stock { get; set; }
+    [Range(1, 9999)]
+    public long Stock { get; set; }
 
     [Required(ErrorMessage = "The Category is Required")]
-    public override int? CategoryId { get; set; }
-
-    [JsonIgnore]
-    public override DateTime? CreatedAt { get; set; }
+    public int CategoryId { get; set; }
 }
--- Modules/Product/DTOs/ProductDTO.cs
+++ Modules/Product/DTOs/ProductDTO.cs
@@ -1,25 +1,20 @@
 namespace ECommerce.Modules.Product;
 
 using ECommerce.Models;
-using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
 public class ProductDTO
 {
     public virtual int? Id { get; set; }
 
-    [MinLength(3), MaxLength(100)]
     public virtual string? Name { get; set; }
 
-    [MinLength(5), MaxLength(255)]
     public virtual string? Description { get; set; }
 
-    [MinLength(5), MaxLength(255)]
     public virtual string? ImageURL { get; set; }
 
     public virtual decimal? Price { get; set; }
 
-    [Range(1, 9999)]
     public virtual long? Stock { get; set; }
 
     public virtual int? CategoryId { get; set; }
