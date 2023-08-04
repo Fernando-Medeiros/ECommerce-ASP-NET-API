2729a8877e14cc69fc629dd8f4a42402ad083a93
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-08-03 21:21:26 -0300

    refactor[module]: Category (DTOs)

--- Modules/Category/DTOs/CategoryDTO.cs
+++ Modules/Category/DTOs/CategoryDTO.cs
@@ -1,18 +1,16 @@
 namespace ECommerce.Modules.Category;
 
-using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 using ECommerce.Models;
 
 public class CategoryDTO
 {
-    public virtual int? Id { get; set; }
+    public int Id { get; set; }
 
-    [MinLength(3), MaxLength(100)]
-    public virtual string? Name { get; set; }
+    public string? Name { get; set; }
 
-    public virtual DateTime? CreatedAt { get; set; }
+    public DateTime? CreatedAt { get; set; }
 
     [JsonIgnore]
-    public virtual ICollection<Product>? Products { get; set; }
+    public ICollection<Product>? Products { get; set; }
 }
