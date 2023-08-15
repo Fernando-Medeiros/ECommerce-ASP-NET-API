f8fa300640833b43894e96e6257ad3b603d86dce
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-08-15 01:26:39 -0300

    feat: CustomerCreateRequest

---
+++ src/Modules/Customer/Requests/CustomerCreateRequest.cs
@@ -0,0 +1,53 @@
+namespace ECommerce.Modules.Customer;
+
+using System.ComponentModel.DataAnnotations;
+
+public class CustomerCreateRequest
+{
+    private string? _name;
+    private string? _firstName;
+    private string? _lastName;
+    private string? _email;
+    private string? _password;
+
+
+    [Required(ErrorMessage = "The Name is Required")]
+    [MinLength(3), MaxLength(50)]
+    public string? Name
+    {
+        get => _name;
+        set => _name = value!.Trim().ToLower();
+    }
+
+    [Required(ErrorMessage = "The FirstName is Required")]
+    [MinLength(3), MaxLength(50)]
+    public string? FirstName
+    {
+        get => _firstName;
+        set => _firstName = value!.Trim().ToLower();
+    }
+
+    [Required(ErrorMessage = "The LastName is Required")]
+    [MinLength(3), MaxLength(50)]
+    public string? LastName
+    {
+        get => _lastName;
+        set => _lastName = value!.Trim().ToLower();
+    }
+
+    [Required(ErrorMessage = "The Email is Required")]
+    [MinLength(11), MaxLength(150), EmailAddress]
+    public string? Email
+    {
+        get => _email;
+        set => _email = value!.Trim();
+    }
+
+    [Required(ErrorMessage = "The Password is Required")]
+    [MinLength(8), MaxLength(255)]
+    public string? Password
+    {
+        get => _password;
+        set => _password = value!.Trim();
+    }
+}
