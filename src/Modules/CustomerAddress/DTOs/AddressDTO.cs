c25d497bfd64a53c5e71e83c5348560fd167d182
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-08-15 12:40:50 -0300

    feat: AddressDTO

---
+++ src/Modules/CustomerAddress/DTOs/AddressDTO.cs
@@ -0,0 +1,19 @@
+namespace ECommerce.Modules.CustomerAddress;
+
+using ECommerce.Models;
+
+public class AddressDTO
+{
+    public string? Id { get; set; }
+    public string? CustomerId { get; set; }
+    public int Number { get; set; }
+    public string? Street { get; set; }
+    public string? ZipCode { get; set; }
+    public string? Type { get; set; }
+    public string? City { get; set; }
+    public string? State { get; set; }
+    public string? Country { get; set; }
+    public DateTime? CreatedAt { get; set; }
+
+    public Customer? Customer { get; set; }
+}
