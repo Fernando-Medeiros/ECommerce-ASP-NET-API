ea40b346609c9308113a45162657645dde98ad16
Fernando Medeiros <97191840+Fernando-Medeiros@users.noreply.github.com>
2023-08-15 12:50:41 -0300

    Feat[Model,Module]: Customer -> prop UpdatedAt (#36)

--- src/Modules/Customer/DTOs/CustomerDTO.cs
+++ src/Modules/Customer/DTOs/CustomerDTO.cs
@@ -9,6 +9,7 @@ public class CustomerDTO
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
     public string? Email { get; set; }
+    public DateTime? UpdatedAt { get; set; }
     public DateTime? CreatedAt { get; set; }
 
     public string? Password { get; set; }
