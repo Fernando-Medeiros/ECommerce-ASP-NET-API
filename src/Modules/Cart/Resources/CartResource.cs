4b0c2125e3bec8c77e2d6f673a72ac172d8dd9ae
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-08-15 22:19:06 -0300

    feat: CartResource

---
+++ src/Modules/Cart/Resources/CartResource.cs
@@ -0,0 +1,29 @@
+namespace ECommerce.Modules.Cart;
+
+public struct CartResource
+{
+    public string? Identify { get; set; }
+    public string? ProductId { get; set; }
+
+    public int Quantity { get; set; }
+    public DateOnly CreatedAt { get; set; }
+
+    public CartResource(CartDTO _)
+    {
+        Identify = _.Id;
+        ProductId = _.ProductId;
+        Quantity = _.Quantity;
+        CreatedAt = DateOnly.FromDateTime(_.CreatedAt!.Value);
+    }
+
+    public static IEnumerable<CartResource> ToArray(
+        IEnumerable<CartDTO?> array)
+    {
+        var carts = new List<CartResource>();
+
+        foreach (var dto in array)
+            if (dto != null) carts.Add(new CartResource(dto));
+
+        return carts;
+    }
+}
