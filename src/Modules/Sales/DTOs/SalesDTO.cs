00adb47f0c62c20f7e2b3a13905dbdf7034897b7
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-08-17 12:48:00 -0300

    refactor: SalesDTO

--- src/Modules/Sales/DTOs/SalesDTO.cs
+++ src/Modules/Sales/DTOs/SalesDTO.cs
@@ -2,17 +2,11 @@ namespace ECommerce.Modules.Sales;
 
 public class SalesDTO
 {
-    public virtual string? Id { get; set; }
-
-    public virtual string? CustomerId { get; set; }
-
-    public virtual string? ProductId { get; set; }
-
-    public virtual decimal? Price { get; set; }
-
-    public virtual int? Quantity { get; set; }
-
-    public virtual DateTime? UpdatedAt { get; set; }
-
-    public virtual DateTime? CreatedAt { get; set; }
+    public string? Id { get; set; }
+    public string? CustomerId { get; set; }
+    public string? ProductId { get; set; }
+    public decimal Price { get; set; }
+    public int Quantity { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public DateTime? CreatedAt { get; set; }
 }
