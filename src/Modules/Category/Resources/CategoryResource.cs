085085a7ac1ff36963344642d1584bcaafc8a6db
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-08-16 21:57:43 -0300

    feat: CategoryResource

---
+++ src/Modules/Category/Resources/CategoryResource.cs
@@ -0,0 +1,26 @@
+namespace ECommerce.Modules.Category;
+
+public struct CategoryResource
+{
+    public string? Identify { get; set; }
+    public string? Name { get; set; }
+    public DateOnly CreatedAt { get; set; }
+
+    public CategoryResource(CategoryDTO _)
+    {
+        Identify = _.Id;
+        Name = _.Name;
+        CreatedAt = DateOnly.FromDateTime(_.CreatedAt!.Value);
+    }
+
+    public static IEnumerable<CategoryResource> ToArray(
+        IEnumerable<CategoryDTO?> array)
+    {
+        var carts = new List<CategoryResource>();
+
+        foreach (var dto in array)
+            if (dto != null) carts.Add(new CategoryResource(dto));
+
+        return carts;
+    }
+}
