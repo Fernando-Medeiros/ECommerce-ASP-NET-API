560c3eeb51f94628b1c7ccd3123e679c1633cb0c
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-08-17 00:01:33 -0300

    feat: ProductService

--- src/Modules/Product/Contracts/IProductService.cs
+++ src/Modules/Product/Contracts/IProductService.cs
@@ -2,13 +2,15 @@ namespace ECommerce.Modules.Product;
 
 public interface IProductService
 {
-    public Task<IEnumerable<ProductDTO>> FindMany(ProductQueryDTO query);
+    public Task<IEnumerable<ProductDTO?>> FindMany(ProductQueryDTO query);
 
-    public Task<ProductDTO> FindOne(string? id = null, string? name = null);
+    public Task<ProductDTO> FindOne(
+        string? id = null,
+        string? name = null);
 
-    public Task Register(ProductDTO product);
+    public Task Register(ProductCreateDTO dto);
 
-    public Task Update(ProductDTO product);
+    public Task Update(ProductUpdateDTO dto);
 
-    public Task Remove(ProductDTO product);
+    public Task Remove(string id);
 }
