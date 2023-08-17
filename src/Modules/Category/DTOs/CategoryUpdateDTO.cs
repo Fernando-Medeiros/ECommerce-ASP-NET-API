586ffe087bdcd8d2d5a3ae7224b6f7aa14bdfcbc
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-08-16 21:54:35 -0300

    feat: CategoryUpdateDTO -> ExtractProprieties & UpdateProperties

--- src/Modules/Category/DTOs/CategoryUpdateDTO.cs
+++ src/Modules/Category/DTOs/CategoryUpdateDTO.cs
@@ -1,10 +1,26 @@
 namespace ECommerce.Modules.Category;
 
-using System.ComponentModel.DataAnnotations;
-
 public class CategoryUpdateDTO
 {
-    [Required(ErrorMessage = "The Name is Required")]
-    [MinLength(3), MaxLength(100)]
+    public string? Id { get; set; }
     public string? Name { get; set; }
+    public DateTime UpdatedAt { get; set; }
+
+    public static CategoryUpdateDTO ExtractProprieties(
+        CategoryUpdateRequest _, string categoryId)
+    {
+        return new()
+        {
+            Id = categoryId,
+            Name = _.Name,
+            UpdatedAt = DateTime.UtcNow,
+        };
+    }
+
+    public void UpdateProperties(ref CategoryDTO category)
+    {
+        if (Name != null) category.Name = Name;
+
+        category.UpdatedAt = UpdatedAt;
+    }
 }
