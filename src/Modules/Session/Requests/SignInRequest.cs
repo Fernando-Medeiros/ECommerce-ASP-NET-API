197dcbd828e236b528c00944ddf2c7e61d0d05fd
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-08-20 18:33:26 -0300

    feat: SignInRequest

---
+++ src/Modules/Session/Requests/SignInRequest.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ECommerce.Modules.Session;
+
+public class SignInRequest
+{
+    private string? _email;
+    private string? _password;
+
+    [MinLength(11), MaxLength(150), EmailAddress, Required]
+    public string? Email
+    {
+        get => _email;
+        set => _email = value?.Trim();
+    }
+
+    [MinLength(8), MaxLength(255), Required]
+    public string? Password
+    {
+        get => _password;
+        set => _password = value?.Trim();
+    }
+}
