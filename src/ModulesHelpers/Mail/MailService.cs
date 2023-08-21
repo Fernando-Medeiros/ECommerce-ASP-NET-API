c2db55e1b6f93f240e421fabeb30a5f58a0e8668
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-08-21 20:59:07 -0300

    feat[module-helper]: MailService -> impl SendTemplateAsync

--- src/ModulesHelpers/Mail/MailService.cs
+++ src/ModulesHelpers/Mail/MailService.cs
@@ -6,17 +6,40 @@ public class MailService : IMailService
 {
     private readonly IFluentEmail _fluentEmail;
 
-    public MailService(IFluentEmail fluentEmail)
-    {
-        _fluentEmail = fluentEmail;
-    }
+    public MailService(IFluentEmail fluentEmail) => _fluentEmail = fluentEmail;
 
     public async Task SendMailAsync(string to, string subject, string body)
     {
+        await _fluentEmail.To(to).Subject(subject).Body(body).SendAsync();
+    }
+
+    public async Task SendTemplateAsync(string to, RecoverPasswordVM model)
+    {
+        await SendAsync(to, "Recover Password", ETemplates.RecoverPassword, model);
+    }
+
+    public async Task SendTemplateAsync(string to, ActiveAccountVM model)
+    {
+        await SendAsync(to, "Active Your Account", ETemplates.ActiveAccount, model);
+    }
+
+    #region Private
+
+    private async Task SendAsync<TModel>(
+        string to, string subject, ETemplates template, TModel model)
+    {
         await _fluentEmail
-            .To(to)
-            .Subject(subject)
-            .Body(body)
+            .To(to).Subject(subject)
+            .UsingTemplateFromFile(ResolvePath(template), model)
             .SendAsync();
     }
+
+    private static string ResolvePath(ETemplates temp)
+    {
+        string path = $"/ModulesHelpers/Mail/Templates/{Enum.GetName(temp)}.cshtml";
+
+        return Directory.GetCurrentDirectory() + path;
+    }
+
+    #endregion
 }
