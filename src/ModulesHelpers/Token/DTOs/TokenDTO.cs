fd2e2ffd67477a0b7b0eb7b4997f2906ac8e1430
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-08-21 16:07:07 -0300

    feat[module]: Token -> TokenDTO -> add Enums

--- src/ModulesHelpers/Token/DTOs/TokenDTO.cs
+++ src/ModulesHelpers/Token/DTOs/TokenDTO.cs
@@ -3,11 +3,16 @@ namespace ECommerce.ModulesHelpers.Token;
 public readonly struct TokenDTO
 {
     public readonly string Token { get; init; }
-    public readonly string Type { get; init; }
+    public readonly ETokenType Type { get; init; }
+    public readonly ETokenScope Scope { get; init; }
 
-    public TokenDTO(string token, string type)
+    public TokenDTO(
+        string token,
+        ETokenType type,
+        ETokenScope scope)
     {
         Token = token;
         Type = type;
+        Scope = scope;
     }
 }
