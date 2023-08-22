0e769c060b5a019c234bb9f5a669512f24d3381b
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-08-22 00:46:32 -0300

    fix[identities]: CustomerIdentity -> CheckTokenScope

--- src/Identities/CustomerIdentity.cs
+++ src/Identities/CustomerIdentity.cs
@@ -1,15 +1,21 @@
-namespace ECommerce.Identities;
-
 using System.Security.Claims;
 using ECommerce.Exceptions;
+using ECommerce.ModulesHelpers.Token;
+
+namespace ECommerce.Identities;
 
 public class CustomerIdentity
 {
     public readonly string Id;
+    public readonly string Scope;
 
     public CustomerIdentity(ClaimsPrincipal principal)
     {
         Id = ExtractProperty(principal, "id").Value;
+
+        Scope = ExtractProperty(principal, "scope").Value;
+
+        CheckTokenScope();
     }
 
     private protected static Claim ExtractProperty(
@@ -18,5 +24,12 @@ public class CustomerIdentity
         return principal.Claims
             .SingleOrDefault(c => c.Type.Equals(keySelector))
             ?? throw new ForbiddenError("Access denied, token incompatible");
+    }
+
+    private protected void CheckTokenScope()
+    {
+        if (Scope != Enum.GetName(ETokenScope.Access) ||
+            Scope != Enum.GetName(ETokenScope.Refresh))
+            throw new UnauthorizedError("Access denied, token scope incompatible");
     }
 }
