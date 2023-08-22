a85d1fa1a144e3ce46f7b0145195377bcf09cdcd
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-08-22 17:17:20 -0300

    feat[module-helper]: static CryptPassword -> [Hash, Verify]

---
+++ src/ModulesHelpers/Crypt/CryptPassword.cs
@@ -0,0 +1,14 @@
+namespace ECommerce.ModulesHelpers.Crypt;
+
+public static class CryptPassword
+{
+    public static string Hash(string password)
+    {
+        return BCrypt.Net.BCrypt.HashPassword(password);
+    }
+
+    public static bool Verify(string password, string passwordHash)
+    {
+        return BCrypt.Net.BCrypt.Verify(password, passwordHash);
+    }
+}
