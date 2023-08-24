b899a789c30950ba780887785e28844e4120b29f
Fernando Medeiros <97191840+Fernando-Medeiros@users.noreply.github.com>
2023-08-24 11:57:46 -0300

    Refactor[module]: rename Address ->> CustomerAddress (#60)

--- src/Modules/CustomerAddress/Contracts/ICustomerAddressRepository.cs
+++ src/Modules/CustomerAddress/Contracts/ICustomerAddressRepository.cs
@@ -1,10 +1,14 @@
 namespace ECommerce.Modules.CustomerAddress;
 
-public interface IAddressRepository
+public interface ICustomerAddressRepository
 {
     public Task<IEnumerable<AddressDTO?>> FindAddresses(string customerId);
+
     public Task<AddressDTO?> FindOne(string addressId, string customerId);
+
     public Task Register(AddressCreateDTO dto);
+
     public Task Update(AddressDTO dto);
+
     public Task Remove(AddressDTO dto);
 }
--- src/Modules/CustomerAddress/Contracts/ICustomerAddressService.cs
+++ src/Modules/CustomerAddress/Contracts/ICustomerAddressService.cs
@@ -1,10 +1,14 @@
 namespace ECommerce.Modules.CustomerAddress;
 
-public interface IAddressService
+public interface ICustomerAddressService
 {
     public Task<IEnumerable<AddressDTO?>> FindAddresses(string customerId);
+
     public Task<AddressDTO> FindOne(string addressId, string customerId);
+
     public Task Register(AddressCreateDTO dto);
+
     public Task Update(AddressUpdateDTO dto);
+
     public Task Remove(string addressId, string customerId);
 }
