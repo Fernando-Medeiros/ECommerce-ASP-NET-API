f0ee978f9e019b8f4a06a09aab4ba32f0a0bdd7f
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-08-24 22:43:00 -0300

    feat[controller]: AuthController -> SwaggerResponses

--- src/Controllers/V1/AuthController.cs
+++ src/Controllers/V1/AuthController.cs
@@ -1,47 +1,45 @@
 using ECommerce.Identities;
 using ECommerce.ModulesHelpers.Token;
+using ECommerce.Startup.SwaggerProducesResponse;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerce.Modules.Auth;
 
-[ApiController]
-[Route("api/v1/auth")]
+[ApiController, Authorize, Route("api/v1/auth")]
+[BadRequest, Unauthorized, Forbidden, NotFound, Created(typeof(TokenResource))]
 public class AuthController : ControllerBase
 {
-    private readonly IAuthService _authService;
+    private readonly IAuthService _service;
 
     public AuthController(
-        IAuthService authService)
-    {
-        _authService = authService;
-    }
+        IAuthService service) => _service = service;
 
-    [HttpPost("signin")]
-    public async Task<ActionResult<TokenResource>> SignIn(
+    [HttpPost("signin"), AllowAnonymous]
+    public async Task<ActionResult> SignIn(
         [FromBody] SignInRequest request)
     {
-        TokenDTO token = await _authService.SignIn(
+        TokenDTO token = await _service.SignIn(
             dto: SignInDTO.ExtractProperties(request));
 
         TokenResource resource = new(token);
 
-        return Ok(resource);
+        return Created("", resource);
     }
 
-    [Authorize]
+
     [HttpPost("authenticate")]
-    public async Task<ActionResult<TokenResource>> AuthenticateEmail(
+    public async Task<ActionResult> AuthenticateEmail(
         [FromBody] SignInRequest request)
     {
         CustomerIdentity customer = new(
             User, new() { ETokenScope.AuthenticateEmail });
 
-        TokenDTO token = await _authService.Authenticate(
+        TokenDTO token = await _service.Authenticate(
             dto: SignInDTO.ExtractProperties(request, customer.Id));
 
         TokenResource resource = new(token);
 
-        return Ok(resource);
+        return Created("", resource);
     }
 }
