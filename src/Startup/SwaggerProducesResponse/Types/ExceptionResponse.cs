16a3a71e945498bddc20b13f50d7fc7e221be135
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-08-24 22:33:47 -0300

    feat[build]: ExceptionResponse

---
+++ src/Startup/SwaggerProducesResponse/Types/ExceptionResponse.cs
@@ -0,0 +1,15 @@
+namespace ECommerce.Startup.SwaggerProducesResponse;
+
+public class ExceptionResponse
+{
+    public string Message { get; set; }
+    public int Status { get; set; }
+    public DateTime CreatedAt { get; set; }
+
+    public ExceptionResponse(string message, int status)
+    {
+        Message = message;
+        Status = status;
+        CreatedAt = DateTime.UtcNow;
+    }
+}
