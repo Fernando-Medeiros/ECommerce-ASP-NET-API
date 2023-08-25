368b58d0f39c01fd5bdbbc19df81fcc0019e22df
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-08-24 22:40:17 -0300

    feat[exception]: Errors -> add ExceptionResponse

--- src/Exceptions/Errors/ForbiddenError.cs
+++ src/Exceptions/Errors/ForbiddenError.cs
@@ -1,6 +1,9 @@
+using ECommerce.Startup.SwaggerProducesResponse;
+
 namespace ECommerce.Exceptions;
 
 public class ForbiddenError : HttpResponseException
 {
-    public ForbiddenError(string? message) : base(403, new { message }) { }
+    public ForbiddenError(string message)
+        : base(403, new ExceptionResponse(message, 403)) { }
 }
--- src/Exceptions/Errors/UnauthorizedError.cs
+++ src/Exceptions/Errors/UnauthorizedError.cs
@@ -1,6 +1,9 @@
+using ECommerce.Startup.SwaggerProducesResponse;
+
 namespace ECommerce.Exceptions;
 
 public class UnauthorizedError : HttpResponseException
 {
-    public UnauthorizedError(string? message) : base(401, new { message }) { }
+    public UnauthorizedError(string message)
+        : base(401, new ExceptionResponse(message, 401)) { }
 }
