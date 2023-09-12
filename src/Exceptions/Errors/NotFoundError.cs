7e59d0b6a93640390dac43f63360143186d5f376
Fernando Medeiros <97191840+Fernando-Medeiros@users.noreply.github.com>
2023-09-12 19:12:01 -0300

    Refactor[exceptions]: rename (abstract)HttpException (#73)

--- src/Exceptions/Errors/NotFoundError.cs
+++ src/Exceptions/Errors/NotFoundError.cs
@@ -1,8 +1,6 @@
-using ECommerce.Setup.ApiProducesResponse;
-
 namespace ECommerce.Exceptions;
 
-public class NotFoundError : HttpResponseException
+public class NotFoundError : HttpException
 {
     public NotFoundError(string message)
         : base(404, new ExceptionResponse(message, 404)) { }
--- src/Exceptions/Errors/UnauthorizedError.cs
+++ src/Exceptions/Errors/UnauthorizedError.cs
@@ -1,8 +1,6 @@
-using ECommerce.Setup.ApiProducesResponse;
-
 namespace ECommerce.Exceptions;
 
-public class UnauthorizedError : HttpResponseException
+public class UnauthorizedError : HttpException
 {
     public UnauthorizedError(string message)
         : base(401, new ExceptionResponse(message, 401)) { }
