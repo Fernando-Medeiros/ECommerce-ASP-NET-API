fd205fca6b73a160499232204fb7e6b0a94a1d09
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-09-12 18:00:41 -0300

    refactor[context][customer]: CustomerRepository

--- src/Modules/Customer/Contracts/ICustomerRepository.cs
+++ src/Modules/Customer/Contracts/ICustomerRepository.cs
@@ -2,11 +2,11 @@ namespace ECommerce.Modules.Customer;
 
 public interface ICustomerRepository
 {
-    public Task<CustomerDTO?> Find(string? id = null, string? email = null);
+    public Task<CustomerDTO?> Find(CustomerDTO dto);
 
-    public Task<CustomerDTO> Register(CustomerCreateDTO dto);
+    public Task<CustomerDTO> Register(CustomerDTO customer);
 
-    public Task Update(CustomerDTO dto);
+    public Task Update(CustomerDTO customer);
 
-    public Task Remove(CustomerDTO dto);
+    public Task Remove(CustomerDTO customer);
 }
