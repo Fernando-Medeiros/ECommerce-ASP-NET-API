8f09bad53c5882bce7805194bb47b45cba8f0262
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-09-12 18:03:38 -0300

    feat[module][customer]: (usecase) UpdateCustomer

---
+++ src/Modules/Customer/UseCases/UpdateCustomer.cs
@@ -0,0 +1,34 @@
+using AutoMapper;
+
+namespace ECommerce.Modules.Customer;
+
+public class UpdateCustomer
+{
+    private readonly ICustomerRepository _repository;
+    private readonly IMapper _mapper;
+
+    public UpdateCustomer(
+        ICustomerRepository repository,
+        IMapper mapper)
+    {
+        _repository = repository;
+        _mapper = mapper;
+    }
+
+    public async Task Execute(CustomerUpdateDTO dto)
+    {
+        var currentCustomer = await _repository
+            .FindOneOrNotFound(new() { Id = dto.Id });
+
+        if (dto.Email != currentCustomer.Email)
+            await _repository.UniqueEmailConstraint(dto.Email);
+
+        var customerDomain = new CustomerDomain()
+            .LoadState(currentCustomer)
+            .Update(dto);
+
+        var customerDto = _mapper.Map<CustomerDTO>(customerDomain);
+
+        await _repository.Update(customerDto);
+    }
+}
