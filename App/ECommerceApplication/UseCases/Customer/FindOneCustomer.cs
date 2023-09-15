e5a7d6ea64e9824c98a70306190835708d3964cb
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-09-15 18:24:07 -0300

    feat[app][usecases][customer]: FindOneCustomer

---
+++ App/ECommerceApplication/UseCases/Customer/FindOneCustomer.cs
@@ -0,0 +1,21 @@
+using ECommerceApplication.Contracts;
+using ECommerceApplication.Exceptions;
+using ECommerceDomain.DTOs;
+
+namespace ECommerceApplication.UseCases.Customer;
+
+public sealed class FindOneCustomer : IUseCase<CustomerDTO, CustomerDTO>
+{
+    private readonly ICustomerRepository _repository;
+
+    public FindOneCustomer(ICustomerRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<CustomerDTO> Execute(CustomerDTO data)
+    {
+        return await _repository.FindOne(data)
+            ?? throw new CustomerNotFoundException();
+    }
+}
