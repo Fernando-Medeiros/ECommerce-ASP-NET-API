b79870c63f87a4c60666f842614fa58047616aca
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-09-15 12:31:49 -0300

    feat[domain][exceptions]: EmailFormatException

---
+++ App/ECommerceDomain/Exceptions/EmailFormatException.cs
@@ -0,0 +1,17 @@
+using ECommerceDomain.Abstractions;
+
+namespace ECommerceDomain.Exceptions;
+
+public sealed class EmailFormatException : DomainException
+{
+    public EmailFormatException()
+        : base(
+            status: 404,
+            error: nameof(EmailFormatException),
+            message: "Invalid email format",
+            details: new() {
+                "min:6 max:*",
+                "options: [a-z A-Z 0-9 . _ % + -]",
+                "example: e@e.co"})
+    { }
+}
