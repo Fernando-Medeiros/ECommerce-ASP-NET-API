14315de13b811addfa786c3fa6b29b67708e0eaf
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-09-15 18:24:25 -0300

    feat[app][usecases][customer]: RemoveCustomer

---
+++ App/ECommerceApplication/UseCases/Customer/RemoveCustomer.cs
@@ -0,0 +1,29 @@
+using ECommerceApplication.Contracts;
+using ECommerceApplication.Exceptions;
+using ECommerceDomain.DTOs;
+
+namespace ECommerceApplication.UseCases.Customer;
+
+public sealed class RemoveCustomer : IUseCase<CustomerDTO>
+{
+    private readonly ICustomerRepository _repository;
+    private readonly IUnitTransactionWork _transaction;
+
+    public RemoveCustomer(
+        ICustomerRepository repository,
+        IUnitTransactionWork transaction)
+    {
+        _repository = repository;
+        _transaction = transaction;
+    }
+
+    public async Task Execute(CustomerDTO data)
+    {
+        var customerDto = await _repository.FindOne(data)
+            ?? throw new CustomerNotFoundException();
+
+        _repository.Remove(customerDto);
+
+        await _transaction.Commit();
+    }
+}
