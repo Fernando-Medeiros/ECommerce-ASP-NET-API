3ebfab71a8ee84ff7fda252e2aa189ac979c6f2a
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-09-15 18:18:03 -0300

    fix[app][contracts]: ICryptPassword > Hash (params)

--- App/ECommerceApplication/Contracts/ICryptPassword.cs
+++ App/ECommerceApplication/Contracts/ICryptPassword.cs
@@ -2,7 +2,7 @@ namespace ECommerceApplication.Contracts;
 
 public interface ICryptPassword
 {
-    public string Hash(string? password, string? passwordHash);
+    public string Hash(string? password);
 
     public bool Verify(string? password, string? passwordHash);
 }
