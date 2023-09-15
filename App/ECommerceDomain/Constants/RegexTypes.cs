3c81634a4d518d5dc763f3fe1acde92d708bfc81
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-09-15 12:28:29 -0300

    feat[domain][constants]: RegexTypes

---
+++ App/ECommerceDomain/Constants/RegexTypes.cs
@@ -0,0 +1,10 @@
+namespace ECommerceDomain.Constants;
+
+public static class RegexTypes
+{
+    public const string Email = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+
+    public const string Name = @"^[A-Za-zÀ-ÿ]{3,18}$";
+
+    public const string PasswordHash = @"^\$2[ayb]\$\d{2}\$[./A-Za-z0-9]{53}$";
+}
