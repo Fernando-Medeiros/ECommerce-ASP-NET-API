6b9ab743860cc225d279eaba821c7a6c56902f2a
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-09-16 18:28:09 -0300

    refactor[app][mappers]: CustomerMapper

--- App/ECommerceApplication/Mappers/CustomerMapper.cs
+++ App/ECommerceApplication/Mappers/CustomerMapper.cs
@@ -14,15 +14,15 @@ public class CustomerMapper : Profile
         CreateMap<UpdateCustomerRequest, CustomerDTO>();
 
         CreateMap<CustomerEntity, CustomerDTO>()
-            .ForMember(d => d.Id, m => m.MapFrom(e => e.Id.Value))
-            .ForMember(d => d.Name, m => m.MapFrom(e => e.Name.Name.Value))
-            .ForMember(d => d.FirstName, m => m.MapFrom(e => e.Name.FirstName.Value))
-            .ForMember(d => d.LastName, m => m.MapFrom(e => e.Name.LastName.Value))
-            .ForMember(d => d.Email, m => m.MapFrom(e => e.Email.Value))
-            .ForMember(d => d.Password, m => m.MapFrom(e => e.Password.Value))
-            .ForMember(d => d.Role, m => m.MapFrom(e => e.Role.Value))
-            .ForMember(d => d.CreatedAt, m => m.MapFrom(e => e.CreatedAt))
-            .ForMember(d => d.UpdatedAt, m => m.MapFrom(e => e.UpdatedAt))
-            .ForMember(d => d.VerifiedAt, m => m.MapFrom(e => e.VerifiedAt));
+            .ForMember(d => d.Id, m => m.MapFrom(e => e.Id!.Value))
+            .ForMember(d => d.Name, m => m.MapFrom(e => e.Name!.Name.Value))
+            .ForMember(d => d.FirstName, m => m.MapFrom(e => e.Name!.FirstName.Value))
+            .ForMember(d => d.LastName, m => m.MapFrom(e => e.Name!.LastName.Value))
+            .ForMember(d => d.Email, m => m.MapFrom(e => e.Email!.Value))
+            .ForMember(d => d.Password, m => m.MapFrom(e => e.Password!.Value))
+            .ForMember(d => d.Role, m => m.MapFrom(e => e.Role!.Value))
+            .ForMember(d => d.CreatedOn, m => m.MapFrom(e => e.CreatedOn!.Value))
+            .ForMember(d => d.UpdatedOn, m => m.MapFrom(e => e.UpdatedOn!.Value))
+            .ForMember(d => d.VerifiedOn, m => m.MapFrom(e => e.VerifiedOn!.Value));
     }
 }
