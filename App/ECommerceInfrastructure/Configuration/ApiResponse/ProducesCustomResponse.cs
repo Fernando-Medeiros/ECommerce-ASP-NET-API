d9cad0f906873b1582f99dd5601c640f5f124db9
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-09-16 01:30:19 -0300

    feat[infra][config][api]: ProducesCustomResponse [200 ~ 500]

---
+++ App/ECommerceInfrastructure/Configuration/ApiResponse/ProducesCustomResponse.cs
@@ -0,0 +1,42 @@
+using ECommerceDomain.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceInfrastructure.Configuration.ApiResponse;
+
+public abstract class ProducesCustomResponse : ProducesResponseTypeAttribute
+{
+    public ProducesCustomResponse(
+        int statusCode, Type? type = null)
+        : base(type ?? typeof(DomainResponseException), statusCode) { }
+}
+
+public class Success : ProducesCustomResponse
+{ public Success(Type type) : base(200, type) { } }
+
+
+public class Created : ProducesCustomResponse
+{ public Created(Type? type = null) : base(201, type) { } }
+
+
+public class NoContent : ProducesCustomResponse
+{ public NoContent() : base(204) { } }
+
+
+public class BadRequest : ProducesCustomResponse
+{ public BadRequest() : base(400) { } }
+
+
+public class Unauthorized : ProducesCustomResponse
+{ public Unauthorized() : base(401) { } }
+
+
+public class Forbidden : ProducesCustomResponse
+{ public Forbidden() : base(403) { } }
+
+
+public class NotFound : ProducesCustomResponse
+{ public NotFound() : base(404) { } }
+
+
+public class InternalError : ProducesCustomResponse
+{ public InternalError() : base(500) { } }
