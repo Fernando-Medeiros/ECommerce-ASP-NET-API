cec20bfd8d4736c875b3bd32656fd91f9142262d
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-09-16 01:22:04 -0300

    feat[infra][auth]: ITokenService

---
+++ App/ECommerceInfrastructure/Authentication/Tokens/Contracts/ITokenService.cs
@@ -0,0 +1,10 @@
+using ECommerceDomain.DTOs;
+using ECommerceInfrastructure.Authentication.Tokens.DTOs;
+using ECommerceInfrastructure.Authentication.Tokens.Enums;
+
+namespace ECommerceInfrastructure.Authentication.Tokens.Contracts;
+
+public interface ITokenService
+{
+    public TokenDTO Generate(CustomerDTO dto, ETokenScopes scope);
+}
