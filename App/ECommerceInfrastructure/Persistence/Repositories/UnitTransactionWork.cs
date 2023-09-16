879fa74c7e9850681290801c19bdfe940e93f20b
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-09-16 01:26:44 -0300

    feat[infra][data][repository]: UnitTransactionWork

---
+++ App/ECommerceInfrastructure/Persistence/Repositories/UnitTransactionWork.cs
@@ -0,0 +1,16 @@
+using ECommerceApplication.Contracts;
+using ECommerceInfrastructure.Persistence.Contexts;
+
+namespace ECommerceInfrastructure.Persistence.Repositories;
+
+public class UnitTransactionWork : IUnitTransactionWork
+{
+    private readonly DatabaseContext _context;
+
+    public UnitTransactionWork(DatabaseContext context) => _context = context;
+
+    public async Task Commit()
+    {
+        await _context.SaveChangesAsync();
+    }
+}
