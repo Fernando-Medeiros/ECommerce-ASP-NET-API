7307136a95baf8c9471f26e19a3b5b91b8b2bd3d
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-09-16 01:29:41 -0300

    feat[infra][config][env]: Environment

---
+++ App/ECommerceInfrastructure/Configuration/Environment/DatabaseEnvironment.cs
@@ -0,0 +1,12 @@
+namespace ECommerceInfrastructure.Configuration.Environment;
+
+public static class DatabaseEnvironment
+{
+    public static string? ConnectionString { get; private set; }
+
+    public static void LoadEnv(IConfiguration x)
+    {
+        ConnectionString = x.GetValue<string>("DB_DEFAULT_CONNECTION");
+    }
+
+}
---
+++ App/ECommerceInfrastructure/Configuration/Environment/RedirectEnvironment.cs
@@ -0,0 +1,13 @@
+namespace ECommerceInfrastructure.Configuration.Environment;
+
+public static class RedirectEnvironment
+{
+    public static string? AuthEmailURL { get; private set; }
+    public static string? ResetPasswordURL { get; private set; }
+
+    public static void LoadEnv(IConfiguration x)
+    {
+        AuthEmailURL = x.GetValue<string>("URL_AUTH_EMAIL");
+        ResetPasswordURL = x.GetValue<string>("URL_RESET_PASS");
+    }
+}
