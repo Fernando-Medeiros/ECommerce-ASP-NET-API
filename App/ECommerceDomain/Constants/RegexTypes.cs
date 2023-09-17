ca17218246d7ac71166a819cc169ffdc20bcebce
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-09-16 22:32:23 -0300

    fix[domain][constants]: RegexTypes > Name [ex: 'john', 'john dee']

--- App/ECommerceDomain/Constants/RegexTypes.cs
+++ App/ECommerceDomain/Constants/RegexTypes.cs
@@ -4,7 +4,7 @@ public static class RegexTypes
 {
     public const string Email = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
 
-    public const string Name = @"^[A-Za-zÀ-ÿ]{3,18}$";
+    public const string Name = @"^(?!.*([A-Za-zÀ-ÿ])\1\1)[A-Za-zÀ-ÿ]{3,18}(?: [A-Za-zÀ-ÿ]{3,18})?$";
 
     public const string PasswordHash = @"^\$2[ayb]\$\d{2}\$[./A-Za-z0-9]{53}$";
 }
