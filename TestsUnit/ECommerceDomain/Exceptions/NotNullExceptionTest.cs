ecdaa96775c1247c040fff0a652180dff9b3b383
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-09-21 17:45:29 -0300

    feat[domain][tests][unit]: NotNullExceptionTest

---
+++ TestsUnit/ECommerceDomain/Exceptions/NotNullExceptionTest.cs
@@ -0,0 +1,29 @@
+using ECommerceDomain.Exceptions;
+
+namespace TestsUnit.ECommerceDomain.Exceptions;
+
+public class NotNullExceptionTest
+{
+    [Fact]
+    public void Should_Create_Exception()
+    {
+        var result = new NotNullException("Name")
+            .Target(nameof(NotNullExceptionTest));
+
+        Assert.NotNull(result.Value.Message);
+        Assert.Equal(400, result.Value.StatusCode);
+        Assert.Contains("Name", result.Message);
+        Assert.Equal(nameof(NotNullException), result.Value.Error);
+        Assert.Equal(nameof(NotNullExceptionTest), result.Value.Target);
+    }
+
+    [Fact]
+    public void Should_Throw_Exception()
+    {
+        Assert.ThrowsAny<NotNullException>(() =>
+        {
+            throw new NotNullException(target: "Name")
+                .Target(nameof(NotNullExceptionTest));
+        });
+    }
+}
