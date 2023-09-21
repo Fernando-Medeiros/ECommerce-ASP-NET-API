1cc4fba28363898fa5482f185a719dee2eeab22d
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-09-21 17:33:56 -0300

    feat[domain][constants]: RegexTypes > Password

--- App/ECommerceDomain/Constants/RegexTypes.cs
+++ App/ECommerceDomain/Constants/RegexTypes.cs
@@ -6,5 +6,7 @@ public static class RegexTypes
 
     public const string Name = @"^(?!.*([A-Za-zÀ-ÿ])\1\1)[A-Za-zÀ-ÿ]{3,18}(?: [A-Za-zÀ-ÿ]{3,18})?$";
 
+    public const string Password = @"^(?=.*[a-zA-Z0-9çÇ@._-])[a-zA-Z0-9çÇ@._-]{8,16}$";
+
     public const string PasswordHash = @"^\$2[ayb]\$\d{2}\$[./A-Za-z0-9]{53}$";
 }
