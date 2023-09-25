ac60a04a94cd9921caf0259cfa576d81e0a8c673
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-09-25 17:36:48 -0300

    refactor[domain][test][unit]: CustomerEntityTest > add CustomerMocks

--- Test.Unit/ECommerceDomain/Entities/CustomerEntityTest.cs
+++ Test.Unit/ECommerceDomain/Entities/CustomerEntityTest.cs
@@ -1,102 +1,86 @@
 using ECommerceDomain.DTOs;
 using ECommerceDomain.Entities;
-using ECommerceDomain.Enums;
+using Test.Setup.Mocks;
 
 namespace Test.Unit.ECommerceDomain.Entities;
 
 public class CustomerEntityTest
 {
-    private readonly CustomerDTO currentState = new()
-    {
-        Id = Guid.NewGuid().ToString(),
-        Name = "John",
-        FirstName = "dee",
-        LastName = "foo",
-        Email = "example@mail.com",
-        Password = BCrypt.Net.BCrypt.HashPassword("test123"),
-        Role = nameof(ERoles.customer),
-        CreatedOn = DateTimeOffset.UtcNow
-    };
+    private readonly CustomerDTO CurrentState = CustomerMocks.Customer;
 
-    private readonly CustomerDTO data = new()
-    {
-        Name = "John",
-        FirstName = "dev",
-        LastName = "woodcutter",
-        Email = "john_deve@mail.com",
-        Password = BCrypt.Net.BCrypt.HashPassword("test777")
-    };
+    private readonly CustomerDTO DataToRegister = CustomerMocks.DataToRegister;
 
+    private readonly CustomerDTO DataToUpdate = CustomerMocks.DataToUpdate;
 
     [Fact]
     public void Should_Register_New_Customer()
     {
-        var en = new CustomerEntity().Register(data);
+        var en = new CustomerEntity().Register(DataToRegister);
 
         Assert.NotNull(en.Id?.Value);
-        Assert.Equal(en.Name!.Name, data.Name);
-        Assert.Equal(en.Name!.FirstName, data.FirstName);
-        Assert.Equal(en.Name!.LastName, data.LastName);
-        Assert.Equal(en.Email!, data.Email);
-        Assert.Equal(en.Password!, data.Password);
+        Assert.Equal(en.Name!.Name, DataToRegister.Name);
+        Assert.Equal(en.Name!.FirstName, DataToRegister.FirstName);
+        Assert.Equal(en.Name!.LastName, DataToRegister.LastName);
+        Assert.Equal(en.Email!, DataToRegister.Email);
+        Assert.Equal(en.Password!, DataToRegister.Password);
     }
 
     [Fact]
     public void Should_Load_Current_State()
     {
-        var en = new CustomerEntity(currentState);
+        var en = new CustomerEntity(CurrentState);
 
-        Assert.Equal(en.Id!, currentState.Id);
-        Assert.Equal(en.Name!.Name, currentState.Name);
-        Assert.Equal(en.Name!.FirstName, currentState.FirstName);
-        Assert.Equal(en.Name!.LastName, currentState.LastName);
-        Assert.Equal(en.Email!, currentState.Email);
-        Assert.Equal(en.Password!, currentState.Password);
-        Assert.StrictEqual(en.CreatedOn!, currentState.CreatedOn);
+        Assert.Equal(en.Id!, CurrentState.Id);
+        Assert.Equal(en.Name!.Name, CurrentState.Name);
+        Assert.Equal(en.Name!.FirstName, CurrentState.FirstName);
+        Assert.Equal(en.Name!.LastName, CurrentState.LastName);
+        Assert.Equal(en.Email!, CurrentState.Email);
+        Assert.Equal(en.Password!, CurrentState.Password);
+        Assert.StrictEqual(en.CreatedOn!, CurrentState.CreatedOn);
     }
 
     [Fact]
     public void Should_Update_Name()
     {
-        var en = new CustomerEntity(currentState).UpdateName(data);
+        var en = new CustomerEntity(CurrentState).UpdateName(DataToUpdate);
 
-        Assert.Equal(en.Name!.Name, data.Name);
-        Assert.Equal(en.Name!.FirstName, data.FirstName);
-        Assert.Equal(en.Name!.LastName, data.LastName);
+        Assert.Equal(en.Name!.Name, DataToUpdate.Name);
+        Assert.Equal(en.Name!.FirstName, DataToUpdate.FirstName);
+        Assert.Equal(en.Name!.LastName, DataToUpdate.LastName);
         Assert.True(en.UpdatedOn! < DateTimeOffset.UtcNow);
     }
 
     [Fact]
     public void Should_Update_Email()
     {
-        var en = new CustomerEntity(currentState).UpdateEmail(data);
+        var en = new CustomerEntity(CurrentState).UpdateEmail(DataToUpdate);
 
-        Assert.Equal(en.Email!, data.Email);
+        Assert.Equal(en.Email!, DataToUpdate.Email);
         Assert.True(en.UpdatedOn! < DateTimeOffset.UtcNow);
     }
 
     [Fact]
     public void Should_Update_Password()
     {
-        var en = new CustomerEntity(currentState).UpdatePassword(data);
+        var en = new CustomerEntity(CurrentState).UpdatePassword(DataToUpdate);
 
-        Assert.Equal(en.Password!, data.Password);
+        Assert.Equal(en.Password!, DataToUpdate.Password);
         Assert.True(en.UpdatedOn! < DateTimeOffset.UtcNow);
     }
 
     [Fact]
     public void Should_Update_Role()
     {
-        var en = new CustomerEntity(currentState).UpdateRole(data);
+        var en = new CustomerEntity(CurrentState).UpdateRole(DataToUpdate);
 
-        Assert.Equal(en.Role!, data.Role);
+        Assert.Equal(en.Role!, DataToUpdate.Role);
         Assert.True(en.UpdatedOn! < DateTimeOffset.UtcNow);
     }
 
     [Fact]
     public void Should_Assign_Verified()
     {
-        var en = new CustomerEntity(currentState).AssignVerified();
+        var en = new CustomerEntity(CurrentState).AssignVerified();
 
         Assert.True(en.VerifiedOn! < DateTimeOffset.UtcNow);
         Assert.True(en.UpdatedOn! < DateTimeOffset.UtcNow);
