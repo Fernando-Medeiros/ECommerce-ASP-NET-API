9529e604d19815129bf55013f6d8c18575a592b7
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-09-26 22:50:38 -0300

    feat[test][setup]: (abstract) SharedCustomerTest

---
+++ Test.Setup/Shared/SharedCustomerTest.cs
@@ -0,0 +1,21 @@
+using ECommerceApplication.Contracts;
+using ECommerceDomain.DTOs;
+using NSubstitute;
+using Test.Setup.Fixtures;
+using Test.Setup.Mocks;
+
+namespace Test.Setup.Shared;
+
+public abstract class SharedCustomerTest : CustomerMocks
+{
+    public readonly ICustomerRepository _repository = Substitute.For<ICustomerRepository>();
+
+    public readonly RequestFixture _requestFixture = new("/api/v1/customers");
+
+    public readonly ResponseFixture _responseFixture = new();
+
+    public void MakeRepositoryStub(CustomerDTO input, CustomerDTO? output)
+    {
+        _repository.FindOne(Arg.Is(input)).Returns(output);
+    }
+}
