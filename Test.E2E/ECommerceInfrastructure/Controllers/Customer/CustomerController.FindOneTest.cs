e6be5ee7964cfa674bf3a0ee89d00c19a655cc13
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-09-28 13:22:59 -0300

    feat[infra][test][e2e]: CustomerControllerFindOneTest

---
+++ Test.E2E/ECommerceInfrastructure/Controllers/Customer/CustomerController.FindOneTest.cs
@@ -0,0 +1,48 @@
+using System.Net;
+using ECommerceApplication.Exceptions;
+using ECommerceInfrastructure.Presentation.Resources;
+using Test.Setup.Fixtures;
+using Test.Setup.Shared;
+
+namespace Test.E2E.ECommerceInfrastructure.Controllers;
+
+public sealed class CustomerControllerFindOneTest : SharedCustomerTest
+{
+    [Fact]
+    public async void Should_Return_CustomerResource()
+    {
+        using var app = new ServerFixtureE2E(table: ETable.customer);
+
+        await app.InsertOneAsync(Mock.CustomerEntity);
+
+        var response = await app.Client.SendAsync(
+            _requestFixture
+            .FakeAuthorizationHeader(Mock.CustomerDTO)
+            .CreateRequest(HttpMethod.Get)
+        );
+
+        var responseContent = await _responseFixture
+            .Deserialize<CustomerResource?>(response);
+
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.Equivalent(Mock.CustomerResource, responseContent);
+    }
+
+    [Fact]
+    public async void Should_Return_NotFound_Response()
+    {
+        using var app = new ServerFixture();
+
+        var response = await app.Client.SendAsync(
+            _requestFixture
+            .FakeAuthorizationHeader(Mock.CustomerDTO)
+            .CreateRequest(HttpMethod.Get)
+        );
+
+        var responseContent = await _responseFixture
+            .Deserialize<CustomerNotFoundException?>(response);
+
+        Assert.Equal(HttpStatusCode.NotFound, response?.StatusCode);
+        Assert.Equal(nameof(CustomerNotFoundException), responseContent?.Value.Error);
+    }
+}
