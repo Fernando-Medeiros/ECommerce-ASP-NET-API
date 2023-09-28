082b9e3f6f9864b4140cb7852e18a12d7eb7b01a
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-09-28 13:19:43 -0300

    feat[test][setup]: Fixture > ServerFixtureE2E

---
+++ Test.Setup/Fixtures/ServerFixtureE2E.cs
@@ -0,0 +1,45 @@
+using ECommerceInfrastructure.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Test.Setup.Fixtures;
+
+public enum ETable
+{
+    customer = 0
+}
+
+public sealed class ServerFixtureE2E : ServerFixture
+{
+    readonly DatabaseContext _context;
+
+    readonly ETable _table;
+
+    public ServerFixtureE2E(ETable table) : base()
+    {
+        _table = table;
+        _context = _server.Services.GetService<DatabaseContext>()!;
+    }
+
+    public override async void Dispose()
+    {
+        await RemoveOneAsync();
+        base.Dispose();
+    }
+
+    public async Task InsertOneAsync<T>(T entity) where T : class
+    {
+        _context.Add(entity);
+        await _context.SaveChangesAsync();
+    }
+
+    private async Task RemoveOneAsync()
+    {
+        var _ = _table switch
+        {
+            ETable.customer => await _context.Customers.ExecuteDeleteAsync(),
+            _ => 0
+        };
+        await _context.SaveChangesAsync();
+    }
+}
