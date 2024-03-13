8ca7434fe38ccaadbdf0c30f6f6527da8fff9fb3
Fernando Medeiros <97191840+Fernando-Medeiros@users.noreply.github.com>
2024-03-13 09:58:31 -0300

    Fix[Application]: UseCase (#105)

--- App/ECommerceApplication/UseCase/FindOneCustomer.cs
+++ App/ECommerceApplication/UseCase/FindOneCustomer.cs
@@ -14,6 +14,7 @@ public sealed class FindOneCustomer(ICustomerRepository repository)
         CancellationToken cancellationToken = default)
     {
         return await _repository.Find(new(Id: req.Id), cancellationToken)
+
             ?? throw new CustomerNotFoundException().Target(nameof(FindOneCustomer));
     }
 }
