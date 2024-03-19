7cf16d9d549d50e7e10fd68686ca20cab4f01e0f
Fernando Medeiros <97191840+Fernando-Medeiros@users.noreply.github.com>
2024-03-19 16:38:31 -0300

    Refactor[application]: contracts (#112)

--- App/ECommerceApplication/Contract/IAddressRepository.cs
+++ App/ECommerceApplication/Contract/IAddressRepository.cs
@@ -2,9 +2,16 @@ using ECommerceDomain.DTO;
 
 namespace ECommerceApplication.Contract;
 
-public interface IAddressRepository : IRepository<AddressDTO>
+public interface IAddressRepository
 {
     public Task<IEnumerable<AddressDTO>> FindMany(
         AddressDTO dto,
         CancellationToken cancellationToken);
+
+    public Task<AddressDTO?> Find(
+        AddressDTO dto,
+        CancellationToken cancellationToken = default);
+    public void Register(AddressDTO dto);
+    public void Update(AddressDTO dto);
+    public void Remove(AddressDTO dto);
 }
--- App/ECommerceApplication/Contract/ICustomerRepository.cs
+++ App/ECommerceApplication/Contract/ICustomerRepository.cs
@@ -2,4 +2,12 @@ using ECommerceDomain.DTO;
 
 namespace ECommerceApplication.Contract;
 
-public interface ICustomerRepository : IRepository<CustomerDTO> { }
+public interface ICustomerRepository
+{
+    public Task<CustomerDTO?> Find(
+        CustomerDTO dto,
+        CancellationToken cancellationToken = default);
+    public void Register(CustomerDTO dto);
+    public void Update(CustomerDTO dto);
+    public void Remove(CustomerDTO dto);
+}
