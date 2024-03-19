e9371c42730bab6226fa431e1754e262d2d21223
Fernando-Medeiros <fernandomedeiros3k@gmail.com>
2024-03-19 16:36:05 -0300

    Refactor[application]: contracts

--- App/ECommerceApplication/Contract/IRepository.cs
+++
@@ -1,9 +0,0 @@
-namespace ECommerceApplication.Contract;
-
-public interface IRepository<T>
-{
-    public Task<T?> Find(T dto, CancellationToken cancellationToken = default);
-    public void Register(T dto);
-    public void Update(T dto);
-    public void Remove(T dto);
-}
